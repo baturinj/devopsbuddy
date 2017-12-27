70178e9dacdcb08a9be57414cd5628281dd500da
Baturin Jenő <jeno.baturin@gmail.com>
2017-12-27 23:56:58 +0100

    all

---
+++ src/main/java/com/devopsbuddy/config/i18nConfig.java
@@ -0,0 +1,24 @@
+package com.devopsbuddy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+/**
+ * Created by Apache on 2017.12.27..
+ */
+
+@Configuration
+public class i18nConfig {
+
+    @Bean
+    public ReloadableResourceBundleMessageSource messageSource(){
+        ReloadableResourceBundleMessageSource resourceBundleMessageSource = new ReloadableResourceBundleMessageSource();
+        resourceBundleMessageSource.setBasename("classpath:i18n/messages");
+        resourceBundleMessageSource.setCacheSeconds(1800);
+        return resourceBundleMessageSource;
+
+    }
+
+
+}
--- src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
+++ src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
@@ -1,16 +1,37 @@
 package com.devopsbuddy;
 
+import com.devopsbuddy.web.i18n.I18NService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-@RunWith(SpringRunner.class)
+
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
+@WebAppConfiguration
 public class DevopsbuddyApplicationTests {
 
 	@Test
 	public void contextLoads() {
 	}
+
+	@Autowired
+	private I18NService i18NService;
+
+	@Test
+	public void testMessageByLocalService() throws Exception {
+
+		String expectResult = "Bootstrap starter template";
+		String messageId = "index.main.callout";
+		String actual = i18NService.getMessage(messageId);
+		Assert.assertEquals("The actual and expected String don't match", expectResult,actual);
+
+	}
+
+
 
 }
